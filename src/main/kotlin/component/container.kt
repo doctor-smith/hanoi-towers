20d2f898ea7d0c1cd5f94358eab0b6abd5800319
Dr. Florian Schmidt <schmidt@alpha-structure.com>
2022-04-09 10:44:44 +0200

    Implemented Custom container

---
+++ src/main/kotlin/component/container.kt
@@ -0,0 +1,24 @@
+package component
+
+import androidx.compose.runtime.Composable
+import lib.compose.Markup
+import org.jetbrains.compose.web.css.marginLeft
+import org.jetbrains.compose.web.css.pc
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.ElementScope
+import org.w3c.dom.HTMLElement
+
+@Markup
+@Composable
+@Suppress("FunctionName")
+fun Container(content: @Composable ElementScope<HTMLElement>.()->Unit) {
+    Div({
+        style {
+            width(80.pc)
+            marginLeft(20.pc)
+        }
+    }) {
+        content()
+    }
+}
