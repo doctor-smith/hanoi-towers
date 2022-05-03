c9cfa43dd9c21a6e4217f3a63c90dfa523e6c00b
Dr. Florian Schmidt <schmidt@alpha-structure.com>
2022-05-03 16:57:32 +0200

    Integrate routing

--- src/main/kotlin/hanoi/towers/component/body.kt
+++ src/main/kotlin/hanoi/towers/component/body.kt
@@ -8,13 +8,15 @@ import lib.lens.times
 import hanoi.towers.maxNumberOfSlices
 import kotlinx.browser.document
 import lib.compose.ModalLayer
+import lib.compose.routing.Routing
+import lib.compose.routing.navigate
 import lib.language.*
 import org.jetbrains.compose.web.dom.*
 
 @Markup
 @Composable
 @Suppress("FunctionName")
-fun Body(storage: Storage<AppData>) {
+fun UI(storage: Storage<AppData>) {
 
     val texts = (storage * languageLens).read() as Block
     val mainPageTexts = texts.component("hanoi.mainPage")
@@ -37,41 +39,61 @@ fun Body(storage: Storage<AppData>) {
                 storage * localeLens,
                 texts.component("hanoi.locales")
             )
-            H1 { Text(mainPageTexts["headline"]) }
+            Routing("/") {
+                component {
+                    H1 { Text(mainPageTexts["headline"]) }
 
-            Form(
-                storage * numberOfSlicesLens,
-                storage * numberOfMovesLens,
-                storage * hanoiLens,
-                storage * movesLens,
-                storage * isComputingMovesLens,
-                storage * indexOfCurrentMoveLens,
-                storage * errorLens,
-                mainPageTexts.component("form"),
-                maxNumberOfSlices
-            )
+                    Form(
+                        storage * numberOfSlicesLens,
+                        storage * numberOfMovesLens,
+                        storage * hanoiLens,
+                        storage * movesLens,
+                        storage * isComputingMovesLens,
+                        storage * indexOfCurrentMoveLens,
+                        storage * errorLens,
+                        mainPageTexts.component("form"),
+                        maxNumberOfSlices
+                    )
 
-            Statistics(
-                storage * numberOfSlicesLens,
-                storage * numberOfMovesLens,
-                "statistics" of mainPageTexts
-            )
+                    Statistics(
+                        storage * numberOfSlicesLens,
+                        storage * numberOfMovesLens,
+                        "statistics" of mainPageTexts
+                    )
 
-            Flex {
-                ListOfMoves(
-                    storage * movesLens,
-                    storage * isComputingMovesLens,
-                    "listOfMoves" of mainPageTexts
-                )
-                HanoiVisualization(
-                    storage * movesLens,
-                    storage * hanoiLens,
-                    storage * numberOfMovesLens,
-                    storage * indexOfCurrentMoveLens,
-                    storage * movesPerSecondLens,
-                    storage * isPlayingLens,
-                    "visualization" of mainPageTexts
-                )
+                    Flex {
+                        ListOfMoves(
+                            storage * movesLens,
+                            storage * isComputingMovesLens,
+                            "listOfMoves" of mainPageTexts
+                        )
+                        HanoiVisualization(
+                            storage * movesLens,
+                            storage * hanoiLens,
+                            storage * numberOfMovesLens,
+                            storage * indexOfCurrentMoveLens,
+                            storage * movesPerSecondLens,
+                            storage * isPlayingLens,
+                            "visualization" of mainPageTexts
+                        )
+                    }
+
+                    Button({
+                        onClick { navigate("/test") }
+                    }){
+                        Text("Test Page")
+                    }
+                }
+                route("/test"){
+                    component{
+                        Div{ Text("Test Route") }
+                        Button({
+                            onClick { navigate("/") }
+                        }){
+                            Text("Home")
+                        }
+                    }
+                }
             }
         }
     }
