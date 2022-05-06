93dcc496aa3d230fafe800a14c73e67b05db46fa
Dr. Florian Schmidt <schmidt@alpha-structure.com>
2022-05-06 20:43:55 +0200

    Cosmetics

--- src/main/kotlin/hanoi/towers/component/form.kt
+++ src/main/kotlin/hanoi/towers/component/form.kt
@@ -13,6 +13,9 @@ import lib.lens.Storage
 import lib.maths.toThe
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.NumberInput
+import org.jetbrains.compose.web.events.SyntheticInputEvent
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
 
 @Markup
 @Composable
@@ -28,6 +31,8 @@ fun Form(
     texts: Block,
     maxNumberOfSlices: Int
 ) {
+
+
     Div {
         Label(texts["towerHeight"], id="tower-height")
         NumberInput(
@@ -37,50 +42,83 @@ fun Form(
         ) {
             id("tower-height")
             onInput { event ->
-                numberOfSlices.write(
-                    with(event.value) {
-                        when (this) {
-                            null -> numberOfSlices.read()
-                            else -> with(toInt()) {
-                                if (this <= maxNumberOfSlices) {
-                                    this
-                                } else {
-                                    error.write(
-                                        texts["error.limitedNumberOfSlices"]
-                                            .replace(
-                                                "__MAX_NUMBER_OF_SLICES__",
-                                                "$maxNumberOfSlices"
-                                            )
-                                    )
-                                    numberOfSlices.read()
-                                }
-                            }
-                        }
-                    }
-                )
-
-                numberOfMoves.write((2 toThe numberOfSlices.read()) - 1)
-                hanoi.write(Hanoi(
-                    (1..numberOfSlices.read()).map { it }
-                ))
-                CoroutineScope(Job()).launch {
-                    isComputingMoves.write(true)
-                    promise {
-                        moves.write(
-                            moveTower(
-                                hight = numberOfSlices.read(),
-                                from = Tower.One,
-                                to = Tower.Three,
-                                stack = Tower.Two
-                            )(Moves())
-                        )
-                    }.await()
-                    indexOfCurrentMove.write(0)
-                    isComputingMoves.write(false)
+                CoroutineScope(Job()).launch{
+                    handleEvent(
+                        event,
+                        numberOfSlices,
+                        numberOfMoves,
+                        hanoi,
+                        moves,
+                        isComputingMoves,
+                        indexOfCurrentMove,
+                        error,
+                        texts,
+                        maxNumberOfSlices
+                    )
                 }
             }
         }
         OnError(error)
     }
 }
+
+suspend fun handleEvent(
+    event: SyntheticInputEvent<Number?, HTMLInputElement>,
+    numberOfSlices: Storage<Int>,
+    numberOfMoves: Storage<Int>,
+    hanoi: Storage<Hanoi>,
+    moves: Storage<Moves>,
+    isComputingMoves: Storage<Boolean>,
+    indexOfCurrentMove: Storage<Int>,
+    error: Storage<String?>,
+    texts: Block,
+    maxNumberOfSlices: Int
+) = coroutineScope {
+        promise {
+            numberOfSlices.write(
+                with(event.value) {
+                    when (this) {
+                        null -> numberOfSlices.read()
+                        else -> with(toInt()) {
+                            if (this <= maxNumberOfSlices) {
+                                this
+                            } else {
+                                error.write(
+                                    texts["error.limitedNumberOfSlices"]
+                                        .replace(
+                                            "__MAX_NUMBER_OF_SLICES__",
+                                            "$maxNumberOfSlices"
+                                        )
+                                )
+                                numberOfSlices.read()
+                            }
+                        }
+                    }
+                }
+            )
+        }.then {
+            numberOfMoves.write((2 toThe numberOfSlices.read()) - 1)
+        }.then {
+            hanoi.write(Hanoi(
+                (1..numberOfSlices.read()).map { it }
+            ))
+        }.then {
+            isComputingMoves.write(true)
+        }.then {
+            moves.write(
+                moveTower(
+                    hight = numberOfSlices.read(),
+                    from = Tower.One,
+                    to = Tower.Three,
+                    stack = Tower.Two
+                )(Moves())
+            )
+        }.then {
+            indexOfCurrentMove.write(0)
+        }.then {
+            isComputingMoves.write(false)
+        }
+    }
+
+
 
