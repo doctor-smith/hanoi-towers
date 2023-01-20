c65e681f3ef1850a5074871c37faa7ca3d401e47
drx <schmidt@alpha-structure.com>
2023-01-20 13:28:02 +0100

    [HANOI-TOWERS-T-3, HANOI-TOWERS-T-11] TestPage, CSS Loading spinner

--- src/main/kotlin/hanoi/towers/component/loading.kt
+++ src/main/kotlin/hanoi/towers/component/loading.kt
@@ -8,28 +8,7 @@ import org.jetbrains.compose.web.dom.Div
 @Markup
 @Composable
 @Suppress("FunctionName")
-fun Loading() = Div({
-    style {
-        display(DisplayStyle.Flex)
-        justifyContent(JustifyContent.Center)
-        // TODO("center vertically")
-    }
-}) {
-    Div({
-        style {
-            border {
-                width = 16.px
-                style = LineStyle.Solid
-                color = Color("#f3f3f3")
-            }
-            borderRadius(50.percent)
-            //borderTop()
-            width(120.px)
-            height(120.px)
-            animation("spin") {
-                 // TODO("Animate")
-            }
 
-        }
-    }) {}
-}
+fun Loading() = Div ({
+    classes("loading-page")
+})
