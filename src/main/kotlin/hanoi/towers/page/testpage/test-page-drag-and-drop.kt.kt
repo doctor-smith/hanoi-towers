34ba443dbbbd627dd3f59343c8f83da5a5ba920d
drx <schmidt@alpha-structure.com>
2023-01-22 19:47:51 +0100

    [HANOI-TOWERS-T-13] Drag And Drop TestPage

--- src/main/kotlin/hanoi/towers/page/testpage/test-page-drag-and-drop.kt.kt
+++ src/main/kotlin/hanoi/towers/page/testpage/test-page-drag-and-drop.kt.kt
@@ -1,22 +1,101 @@
 package hanoi.towers.page.testpage
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import hanoi.towers.component.layout.Container
+import hanoi.towers.component.layout.Flex
 import lib.compose.Markup
+import lib.maths.x
 import org.jetbrains.compose.web.attributes.Draggable
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 
 @Markup
 @Composable
 @Suppress("FunctionName")
-fun DragDropTestPage() = Div{
-    Text("hi test page")
-    Div(
-        attrs={
-            draggable(Draggable.True)
+fun DragDropTestPage() {
+
+
+
+    Div {
+        Text("hi test page")
+        Div(
+            attrs = {
+                draggable(Draggable.True)
+            }
+        ) {
+            Text("Drag me around - html d&d")
         }
-    ){
-        Text("Drag me around")
+
+        var left by remember { mutableStateOf<Double>(0.0) }
+        var dX by remember { mutableStateOf<Double>(0.0) }
+        var top by remember { mutableStateOf(0.0) }
+        var dY by remember { mutableStateOf<Double>(0.0) }
+        var x by remember { mutableStateOf<Double>(0.0) }
+        var y by remember { mutableStateOf<Double>(0.0) }
+        var mousePressed by remember { mutableStateOf(false) }
+        Div({
+
+            style {
+                position(Position.Relative)
+                left(left.px)
+                top(top.px)
+                height(30.px)
+                width(100.px)
+                border {
+                    style = LineStyle.Solid
+                    color = Color("black")
+                }
+                cursor(when{
+                    mousePressed ->"grabbing"
+                    else -> "grab"
+
+                })
+            }
+
+            onMouseDown {
+                mousePressed = true
+                x = it.pageX
+                y = it.pageY
+            }
+
+            onMouseMove {
+                if(mousePressed) {
+                    dX = it.pageX - x
+                    dY = it.pageY - y
+                    x = it.pageX
+                    y = it.pageY
+
+                    left += dX
+                    top += dY
+                }
+            }
+            onMouseLeave {
+                if(mousePressed) {
+                    dX = it.pageX - x
+                    dY = it.pageY - y
+                    x = it.pageX
+                    y = it.pageY
+
+                    left += dX
+                    top += dY
+                }
+            }
+            onMouseUp {
+                mousePressed = false
+            }
+        }) {
+            Text("custom")
+        }
+    }
+
+
+    Container {
+        Flex {
+            Div { Text("1") }
+            Div { Text("2") }
+        }
     }
 }
 
