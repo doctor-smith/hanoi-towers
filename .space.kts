0e414565d4f20bd0ba89f33af89e3aef328c9069
Florian Schmidt <schmidt@alpha-structure.com>
2023-01-28 19:55:23 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,12 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Hello World!") {
+    container(displayName = "Say Hello", image = "hello-world")
+}
+job("Build and run tests") {
+   gradlew("amazoncorretto:17-alpine", "build")
+}
