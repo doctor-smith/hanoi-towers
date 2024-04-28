19f71c9a50ddfb71841f7abbfbf1787b8574d623
drx <schmidt@alpha-structure.com>
2024-04-28 09:43:56 +0200

    [HANOI-TOWERS-T-34] Improve scripts

--- script/clean-build-all.sh
+++ script/clean-build-all.sh
@@ -1,9 +1,9 @@
 cd ..
-./gradlew clean
-./gradlew hanoi-frontend:clean
-./gradlew hanoi-backend:clean
+./gradlew clean && \
+./gradlew hanoi-frontend:clean && \
+./gradlew hanoi-backend:clean && \
 
-./gradlew build
-./gradlew hanoi-frontend:build
-./gradlew hanoi-backend:build
+./gradlew build && \
+./gradlew hanoi-frontend:build && \
+./gradlew hanoi-backend:build && \
 ./gradlew hanoi-backend:buildFatJar
--- script/rebuild-frontend.sh
+++ script/rebuild-frontend.sh
@@ -1,5 +1,5 @@
 cd ..
-./gradlew hanoi-frontend:clean
-./gradlew hanoi-frontend:build
-docker-compose down --remove-orphans
+./gradlew hanoi-frontend:clean && \
+./gradlew hanoi-frontend:build && \
+docker-compose down --remove-orphans && \
 docker-compose up -d --build hanoi_frontend
