2d073adcca3a5ebd30645fe87c0e5bda63c9164e
drx <schmidt@alpha-structure.com>
2024-04-28 09:44:23 +0200

    [HANOI-TOWERS-T-34] Force usage of java 17 and fix related stuff

--- hanoi-backend/src/test/kotlin/org/evoleq/math/state/StateTest.kt
+++ hanoi-backend/src/test/kotlin/org/evoleq/math/state/StateTest.kt
@@ -26,6 +26,10 @@ class StateTest {
 
         val stateS = State<Int,Int>(5)
 
-        state.applyTo(stateS)
+        val result = state.applyTo(stateS) runOn 1
+
+        assertEquals("5" x 1, result)
+
+        assertEquals(true, false)
     }
 }
