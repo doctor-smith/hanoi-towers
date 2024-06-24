7f3ff92f37d1b692ce797ad442898b8ecc0d486f
Wimmer Simon <sw207@hdm-stuttgart.de>
2024-06-24 17:34:22 +0200

    adjust detekt.yml

--- script/get_detekt_sha.sh
+++ script/get_detekt_sha.sh
@@ -5,6 +5,12 @@ gh api graphql --field tagName=$DETEKT_RELEASE_TAG --raw-field query='
   query getReleaseAssetDownloadUrl($tagName: String!) {
     repository(name: "detekt", owner: "detekt") {
       release(tagName: $tagName) {
+        releaseAssets(first: 1) {  # Hole bis zu 10 Assets, um zu debuggen
+          nodes {
+            name
+            downloadUrl
+          }
+        }
         tagCommit {
           oid
         }
@@ -13,6 +19,13 @@ gh api graphql --field tagName=$DETEKT_RELEASE_TAG --raw-field query='
   }
 ' > gh_response.json
 
+# Gib die gesamte JSON-Antwort aus, um zu debuggen
+cat gh_response.json
+
 # Verarbeite die JSON-Antwort, um den SHA-Wert zu erhalten
 DETEKT_RELEASE_SHA=$(jq --raw-output '.data.repository.release.tagCommit.oid' gh_response.json)
 echo "Detekt Release SHA: $DETEKT_RELEASE_SHA"
+
+# Verarbeite die JSON-Antwort, um den Download-URL zu erhalten
+DETEKT_DOWNLOAD_URL=$(jq --raw-output '.data.repository.release.releaseAssets.nodes[0].downloadUrl' gh_response.json)
+echo "download_url=$DETEKT_DOWNLOAD_URL"
