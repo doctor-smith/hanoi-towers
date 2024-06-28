c3a592abc053de45b7936641e39182ae460dd5f0
Wimmer Simon <sw207@hdm-stuttgart.de>
2024-06-28 20:56:50 +0200

    update detekt settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,34 +10,14 @@ repositories {
 }
 
 detekt {
-    // Version of detekt that will be used. When unspecified the latest detekt
-    // version found will be used. Override to stay on the same version.
+    // Define Version of detekt that will be used
     toolVersion = "1.23.3"
 
-    // The directories where detekt looks for source files. 
-    // Defaults to `files("src/main/java", "src/test/java", "src/main/kotlin", "src/test/kotlin")`.
+    // Define directories where detekt looks for source files
     source.setFrom("hanoi-frontend/src", "hanoi-backend/src")
 
-    // Builds the AST in parallel. Rules are always executed in parallel. 
-    // Can lead to speedups in larger projects. `false` by default.
-    parallel = false
-
-    // Define the detekt configuration(s) you want to use. 
-    // Defaults to the default detekt configuration.
+    // Define the detekt configuration(s) that should be used
     config.setFrom("config/detekt/detekt.yml")
-
-    // Applies the config files on top of detekt's default config file. `false` by default.
-    buildUponDefaultConfig = true
-
-    // Turns on all the rules. `false` by default.
-    allRules = false
-
-    // Disables all default detekt rulesets and will only run detekt with custom rules
-    // defined in plugins passed in with `detektPlugins` configuration. `false` by default.
-    disableDefaultRuleSets = false
-        
-    // Adds debug output during task execution. `false` by default.
-    debug = false
 
     // If set to `true` the build does not fail when the
     // maxIssues count was reached. Defaults to `false`.
@@ -70,7 +50,4 @@ tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
         md.required.set(true)
         md.outputLocation.set(file("build/reports/detekt.md"))
     }
-
-    //this.jvmTarget = "1.8"
-    //jdkHome.set(file("C:/Program Files/Java/jdk-17"))
 }
